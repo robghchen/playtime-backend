a1ab36382cc11d0e582a51c3d051308ec6018a1b
rob <robertchen234@gmail.com>
2019-01-31 11:38:14 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :create, :update, :destroy]
+      get '/current_user', to: 'auth#show'
+      get '/profile', to: 'users#profile'
+      post '/login', to: 'auth#create'
       resources :posts, only: [:index, :create, :update, :destroy]
       resources :comments, only: [:index, :create, :update, :destroy]
     end
