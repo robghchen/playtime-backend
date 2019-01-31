d7078a3485e53fcc6494d568708789077ae1a699
rob <robertchen234@gmail.com>
2019-01-31 11:57:33 -0500

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :username, uniqueness: { case_sensitive: false }
-  validates :email, uniqueness: { case_sensitive: false }
+  validates :first_name, length: { in: 2..10}
+  validates :last_name, length: { in: 2..10}
+  validates :username, presence: true, length: { in: 2..10}, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :email, :with => /@/
+  validates :city, length: { in: 2..15}
+  validates :school, length: { in: 2..15}
+  validates :work, length: { in: 2..15}
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
