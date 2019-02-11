1b296ea32bddb491504c57d991f21d40cc7424ff
rob <robertchen234@gmail.com>
2019-02-11 12:54:18 -0500

    fixed persisting exp

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::UsersController < ApplicationController
 end
 
   def create
-    @user = User.create(username: params[:username], first_name: params[:first_name], last_name: params[last_name], password: params[:password])
+    @user = User.create(username: params[:username], first_name: params[:first_name], last_name: params[:last_name], password: params[:password])
     if @user.valid?
       @token = JWT.encode({username: @user.username}, 'ja2siDc3kJ')
       render json: { user: @user, jwt: @token }, status: :created
@@ -23,7 +23,7 @@ end
   end
 
   def update
-    @user.update(username: params[:username])
+    @user.update(user_params)
     if @user.save
       render json: @user, status: :accepted
     else
@@ -39,7 +39,7 @@ end
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :first_name, :last_name, :lvl, :exp, :exp_limit, :max_energy, :energy)
   end
 
   def find_user
