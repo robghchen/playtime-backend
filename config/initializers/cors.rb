95a8746a1d1c77d41db0454832e1b2f46d303f01
rob <robertchen234@gmail.com>
2019-04-05 23:14:34 -0400

    fix block

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://playtime-beta.netlify.com'
+    origins '*'
 
     resource '*',
       headers: :any,
