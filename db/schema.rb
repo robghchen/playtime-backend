ffea7d8a45bbfac5b3df09d6f0b0ca97d740abd6
rob <robertchen234@gmail.com>
2019-04-07 19:53:50 -0400

    added ticket_img to seats table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_174322) do
+ActiveRecord::Schema.define(version: 2019_04_06_181509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2019_03_21_174322) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "ticket_img"
     t.index ["event_id"], name: "index_seats_on_event_id"
   end
 
